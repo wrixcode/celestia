30c9634c59a98d7f3846d7b7736608698191f76e
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2024-10-18 13:19:55 +0530

    Refactor Navbar component to add active item highlighting

--- src/utils/Navbar.jsx
+++ src/utils/Navbar.jsx
@@ -9,22 +9,24 @@ const Navbar = () => {
     setActiveItem(item);
   };
   return (
-    <div className="w-full  flex justify-center  items-center">
+      <div className="sticky">
+      <div className="w-full  flex justify-center  items-center ">
       <div className="flex justify-between items-center py-2 px-3  m-6 bg-[#090909] rounded-full shadow-[0_0px_10px_rgba(255,255,255,0.62)]  w-[70%] ">
         {/* Navbar */}
         <nav className="flex justify-between  w-full ">
           <div className="text-3xl font-bold mt-1"> <a href="#home"> <img src={logo} alt="logo" /></a> </div>
           <ul className="flex space-x-6 text-lg font-semibold">
-            <li className={`cursor-pointer hover:-translate-y-1 transition-all duration-300 ease-in-out ${activeItem == "home" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("home")}>Home</li>
-            <li className={`cursor-pointer hover:-translate-y-1 transition-all duration-300 ease-in-out ${activeItem == "about" ? "text-[#F15E22]" : ""}`} onClick={()=> handleActive("about")}>About us</li>
-            <li className={`cursor-pointer hover:-translate-y-1 transition-all duration-300 ease-in-out ${activeItem == "events" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("events")}>Events</li>
-            <li className={`cursor-pointer hover:-translate-y-1 transition-all duration-300 ease-in-out ${activeItem == "teams" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("teams")}>Teams</li>
-            <li className={`cursor-pointer hover:-translate-y-1 transition-all duration-300 ease-in-out ${activeItem == "contact" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("contact")}>Contact</li>
-            <li className={`cursor-pointer hover:-translate-y-1 transition-all duration-300 ease-in-out ${activeItem == "gallery" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("gallery")}>Gallery</li>
+            <li className={`cursor-pointer m-0 hover:-translate-y-1 hover:shadow-[0_4px_4px_0_rgba(255,255,255,0.62)] transition duration-300 ease-in-out ${activeItem == "home" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("home")}>Home</li>
+            <li className={`cursor-pointer hover:-translate-y-1 hover:shadow-[0_4px_4px_0_rgba(255,255,255,0.5)] transition-all duration-300 ease-in-out ${activeItem == "about" ? "text-[#F15E22]" : ""}`} onClick={()=> handleActive("about")}>About</li>
+            <li className={`cursor-pointer hover:-translate-y-1 hover:shadow-[0_4px_4px_0_rgba(255,255,255,0.5)] transition-all duration-300 ease-in-out ${activeItem == "events" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("events")}>Events</li>
+            <li className={`cursor-pointer hover:-translate-y-1 hover:shadow-[0_4px_4px_0_rgba(255,255,255,0.5)] transition-all duration-300 ease-in-out ${activeItem == "team" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("team")}>Team</li>
+            <li className={`cursor-pointer hover:-translate-y-1 hover:shadow-[0_4px_4px_0_rgba(255,255,255,0.5)] transition-all duration-300 ease-in-out ${activeItem == "contact" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("contact")}>Contact</li>
+            <li className={`cursor-pointer hover:-translate-y-1 hover:shadow-[0_4px_4px_0_rgba(255,255,255,0.5)] transition-all duration-300 ease-in-out ${activeItem == "gallery" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("gallery")}>Gallery</li>
           </ul>
         </nav>
       </div>
     </div>
+ </div>
   );
 };
 
