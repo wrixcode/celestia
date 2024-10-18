4f49ce61395f5d1ae64f7ec8c5154eb510a355e9
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2024-10-18 11:41:39 +0530

    Refactor Navbar component to add active item highlighting

--- src/utils/Navbar.jsx
+++ src/utils/Navbar.jsx
@@ -1,23 +1,30 @@
 import logo from "../assets/logo.svg";
+import { useState } from "react";
 
 const Navbar = () => {
+
+  const [activeItem, setActiveItem] = useState("home");
+
+  const handleActive = (item) => {
+    setActiveItem(item);
+  };
   return (
-   <div className="w-full  flex justify-center  items-center">
-     <div className="flex justify-between items-center  m-6 bg-[#090909] rounded-full shadow-[0_4px_10px_rgba(255,255,255,0.1)]  w-[70%] ">
-      {/* Navbar */}
-      <nav className="flex justify-between items-center p-3">
-        <div className="text-3xl font-bold"> <a href="#home"> <img src={logo} alt="logo" /></a> </div>
-        <ul className="flex space-x-6 text-lg font-semibold">
-          <li className="hover:text-orange-500 cursor-pointer">Home</li>
-          <li className="hover:text-orange-500 cursor-pointer">About us</li>
-          <li className="hover:text-orange-500 cursor-pointer">Events</li>
-          <li className="hover:text-orange-500 cursor-pointer">Teams</li>
-          <li className="hover:text-orange-500 cursor-pointer">Contact</li>
-          <li className="hover:text-orange-500 cursor-pointer">Gallery</li>
-        </ul>
-      </nav>
+    <div className="w-full  flex justify-center  items-center">
+      <div className="flex justify-between items-center py-2 px-3  m-6 bg-[#090909] rounded-full shadow-[0_0px_10px_rgba(255,255,255,0.62)]  w-[70%] ">
+        {/* Navbar */}
+        <nav className="flex justify-between  w-full ">
+          <div className="text-3xl font-bold mt-1"> <a href="#home"> <img src={logo} alt="logo" /></a> </div>
+          <ul className="flex space-x-6 text-lg font-semibold">
+            <li className={`cursor-pointer hover:-translate-y-1 transition-all duration-300 ease-in-out ${activeItem == "home" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("home")}>Home</li>
+            <li className={`cursor-pointer hover:-translate-y-1 transition-all duration-300 ease-in-out ${activeItem == "about" ? "text-[#F15E22]" : ""}`} onClick={()=> handleActive("about")}>About us</li>
+            <li className={`cursor-pointer hover:-translate-y-1 transition-all duration-300 ease-in-out ${activeItem == "events" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("events")}>Events</li>
+            <li className={`cursor-pointer hover:-translate-y-1 transition-all duration-300 ease-in-out ${activeItem == "teams" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("teams")}>Teams</li>
+            <li className={`cursor-pointer hover:-translate-y-1 transition-all duration-300 ease-in-out ${activeItem == "contact" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("contact")}>Contact</li>
+            <li className={`cursor-pointer hover:-translate-y-1 transition-all duration-300 ease-in-out ${activeItem == "gallery" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("gallery")}>Gallery</li>
+          </ul>
+        </nav>
+      </div>
     </div>
-   </div>
   );
 };
 
