10ec2f0dc5abab8c3ac1f5f719544842227cc472
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2024-10-19 18:54:04 +0530

    added the new event session

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,31 +1,84 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
 import Navbar from "../utils/Navbar.jsx";
 import celestia_logo from "../assets/logocel.svg"
+import event_image1 from "../assets/images/event1.png";
+import event_image2 from "../assets/images/event2.png";
+import event_image3 from "../assets/images/event3.png";
+
+const events = [
+  { src: event_image1, description: "Description of image 1" },
+  { src: event_image2, description: "Description of image 2" },
+  { src: event_image3, description: "Description of image 3" },
+];
+
 const Home = () => {
   return (
     <>
       <div className="home-container flex justify-center">
-        <div className="home-head space-y-20 m-6 w-[70%]" id="home">
+        <div className="home-head space-y-20 m-6 w-[70%] h-screen" id="home">
           {/* <div className="home-navbar"><Navbar/></div> */}
-          <Navbar/>
-          <div className="welcome-text ">
-            <section className='welcome-head'>
-              Innovate. <span className="text-[#F15E22]">Code</span>.
-              Elevate
-            </section>
-            <section className="welcome-words">
-            Welcome to <b>Celestia</b>,<br></br>
-            where computer science students ,<br></br>
-            collaborate, <b>innovate</b>, and <b>create</b> solutions<br></br>
-            that shape the future of technology
-            </section>
-          </div>
-          <div className="celestia-logo flex justify-end">
-              <img className="celestia-logo-home object-fill " src={celestia_logo}  alt="" />
+          <Navbar />
+          <div className="herosection flex justify-between items-center relative top-10">
+            <div className="welcome-text h-ful flex-col">
+              <section className='welcome-head'>
+                Innovate. <span className="text-[#F15E22]">Code</span>.
+                Elevate
+              </section>
+              <section className="welcome-words">
+                Welcome to <b>Celestia</b>,<br></br>
+                where computer science students ,<br></br>
+                collaborate, <b>innovate</b>, and <b>create</b> solutions<br></br>
+                that shape the future of technology
+              </section>
+            </div>
+            <div className="celestia-logo">
+              <img className="celestia-logo-home object-fill " src={celestia_logo} alt="" />
+            </div>
           </div>
         </div>
       </div>
-      
-      
+      <div className="flex justify-center">
+         <div className="w-[70%]">
+               {/* events */}
+               <div className="events-container w-full m-6">
+            <div className="events-head text-center">
+              <h1 className="font-bold text-center text-5xl"> Recent <span className="text-[#F15E22]  ">Events</span></h1>
+            </div>
+            <div className="events-body mt-12">
+              {/* slides */}
+              <Swiper
+                effect={"coverflow"}
+                grabCursor={true}
+                centeredSlides={true}
+                initialSlide={1}
+                loop={false}
+                slidesPerView={3}
+                speed={500}
+                spaceBetween={40}
+                coverflowEffect={{
+                  rotate: 50,
+                  stretch: 0,
+                  depth: 200,
+                  modifier: 1.5,
+                  slideShadows: true,
+                  scale: 0.7,
+                }}
+                pagination={{ clickable: true }}
+                className="swiper_container rounded-lg"
+              >
+                {events.map((image, index) => (
+                  <SwiperSlide key={index}>
+                    <img src={image.src} alt={`slide_image_${index}`} />
+                  </SwiperSlide>
+                ))}
+              </Swiper>
+            </div>
+          </div>
+         </div>
+      </div>
     </>
   );
 };
--- src/utils/Navbar.jsx
+++ src/utils/Navbar.jsx
@@ -2,31 +2,77 @@ import logo from "../assets/logo.svg";
 import { useState } from "react";
 
 const Navbar = () => {
-
   const [activeItem, setActiveItem] = useState("home");
 
   const handleActive = (item) => {
     setActiveItem(item);
   };
+
   return (
-      <div className="home-navbar sticky">
-      <div className="items-center ">
-      <div className="flex justify-between items-center py-2 px-3   bg-[#090909] rounded-full shadow-[0_0px_10px_rgba(255,255,255,0.62)]">
-        {/* Navbar */}
-        <nav className="flex justify-between  w-full ">
-          <div className="text-3xl font-bold mt-1"> <a href="#home"> <img src={logo} alt="logo" /></a> </div>
-          <ul className="flex space-x-6 text-lg font-semibold">
-            <li className={`cursor-pointer m-0 hover:-translate-y-1 hover:shadow-[0_4px_4px_0_rgba(255,255,255,0.62)] transition duration-300 ease-in-out ${activeItem == "home" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("home")}>Home</li>
-            <li className={`cursor-pointer hover:-translate-y-1 hover:shadow-[0_4px_4px_0_rgba(255,255,255,0.5)] transition-all duration-300 ease-in-out ${activeItem == "about" ? "text-[#F15E22]" : ""}`} onClick={()=> handleActive("about")}>About</li>
-            <li className={`cursor-pointer hover:-translate-y-1 hover:shadow-[0_4px_4px_0_rgba(255,255,255,0.5)] transition-all duration-300 ease-in-out ${activeItem == "events" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("events")}>Events</li>
-            <li className={`cursor-pointer hover:-translate-y-1 hover:shadow-[0_4px_4px_0_rgba(255,255,255,0.5)] transition-all duration-300 ease-in-out ${activeItem == "team" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("team")}>Team</li>
-            <li className={`cursor-pointer hover:-translate-y-1 hover:shadow-[0_4px_4px_0_rgba(255,255,255,0.5)] transition-all duration-300 ease-in-out ${activeItem == "contact" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("contact")}>Contact</li>
-            <li className={`cursor-pointer hover:-translate-y-1 hover:shadow-[0_4px_4px_0_rgba(255,255,255,0.5)] transition-all duration-300 ease-in-out ${activeItem == "gallery" ? "text-[#F15E22]" : ""}`} onClick={()=>handleActive("gallery")}>Gallery</li>
-          </ul>
-        </nav>
+    <div className="home-navbar sticky top-5 z-50">
+      <div className="items-center">
+        <div className="flex justify-between items-center py-2 px-3 bg-[#090909] rounded-full shadow-[0_0px_10px_rgba(255,255,255,0.62)]">
+          {/* Navbar */}
+          <nav className="flex justify-between w-full">
+            <div className="text-3xl font-bold mt-1">
+              <a href="#home">
+                <img src={logo} alt="logo" />
+              </a>
+            </div>
+            <ul className="flex space-x-6 text-lg font-semibold">
+              <li
+                className={`cursor-pointer m-0 hover:-translate-y-1 hover:shadow-[0_4px_4px_0_rgba(255,255,255,0.62)] transition duration-300 ease-in-out ${
+                  activeItem == "home" ? "text-[#F15E22]" : ""
+                }`}
+                onClick={() => handleActive("home")}
+              >
+                Home
+              </li>
+              <li
+                className={`cursor-pointer hover:-translate-y-1 hover:shadow-[0_4px_4px_0_rgba(255,255,255,0.5)] transition-all duration-300 ease-in-out ${
+                  activeItem == "about" ? "text-[#F15E22]" : ""
+                }`}
+                onClick={() => handleActive("about")}
+              >
+                About
+              </li>
+              <li
+                className={`cursor-pointer hover:-translate-y-1 hover:shadow-[0_4px_4px_0_rgba(255,255,255,0.5)] transition-all duration-300 ease-in-out ${
+                  activeItem == "events" ? "text-[#F15E22]" : ""
+                }`}
+                onClick={() => handleActive("events")}
+              >
+                Events
+              </li>
+              <li
+                className={`cursor-pointer hover:-translate-y-1 hover:shadow-[0_4px_4px_0_rgba(255,255,255,0.5)] transition-all duration-300 ease-in-out ${
+                  activeItem == "team" ? "text-[#F15E22]" : ""
+                }`}
+                onClick={() => handleActive("team")}
+              >
+                Team
+              </li>
+              <li
+                className={`cursor-pointer hover:-translate-y-1 hover:shadow-[0_4px_4px_0_rgba(255,255,255,0.5)] transition-all duration-300 ease-in-out ${
+                  activeItem == "contact" ? "text-[#F15E22]" : ""
+                }`}
+                onClick={() => handleActive("contact")}
+              >
+                Contact
+              </li>
+              <li
+                className={`cursor-pointer hover:-translate-y-1 hover:shadow-[0_4px_4px_0_rgba(255,255,255,0.5)] transition-all duration-300 ease-in-out ${
+                  activeItem == "gallery" ? "text-[#F15E22]" : ""
+                }`}
+                onClick={() => handleActive("gallery")}
+              >
+                Gallery
+              </li>
+            </ul>
+          </nav>
+        </div>
       </div>
     </div>
- </div>
   );
 };
 
