de724dd6b917c6ab02f33b4b4a922bdb300a762a
Muhammed Shibil M <95218558+muhammedshibilm@users.noreply.github.com>
2024-10-22 10:03:17 +0530

    Refactor Vision component for improved image display and responsiveness

--- src/utils/Vision.jsx
+++ src/utils/Vision.jsx
@@ -11,7 +11,7 @@ const Vision = () => {
             <div className="vision-content w-full  grid grid-cols-1  grid-rows-3  md:flex ">
                 {
                     vision.map((item, index) => (
-                        <div className={`cards bg-[#D9D9D9] text-black rounded-xl mx-20 my-10 md:my-0 md:mx-16 ${index === 1 && 'md:scale-125'}`} key={index}>
+                        <div className={`cards bg-[#D9D9D9] text-black rounded-xl mx-5 my-10 md:my-0 md:mx-16 ${index === 1 && 'md:scale-125'}`} key={index}>
                             <h1 className="font-bold text-3xl py-10 text-center">{item.title}</h1>
                             <p className="pb-16 px-10 text-center ">{item.dec}</p>
                         </div>
@@ -24,4 +24,3 @@ const Vision = () => {
 }
 
 export default Vision;
-
