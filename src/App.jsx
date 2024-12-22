e538d842313046ea050b718e966a6c13a35bc253
wrixcode <wrixcode@gmail.com>
2024-12-22 17:56:58 +0530

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
+import { ToastContainer } from "react-toastify"
 import Home from "./pages/Home"
+import Contact from "./pages/Contact"
+import Team from "./pages/Team"
+import Gallery from "./pages/Gallery"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <Home/>
+    <ToastContainer/>
+  
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route  path="/Contact" element={<Contact/>} />
+        <Route path="/Team" element={<Team />} />
+        <Route path="/Gallery" element={<Gallery />} />
+      </Routes>
+    </Router>
     </>
   )
 }
