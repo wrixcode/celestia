abf6d7c954b209dbf46f79873472c1c40c9e5ea3
sqadirkvm <sarhanqadir007@gmail.com>
2025-01-02 14:43:57 +0530

    Fixed the footer of about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -41,13 +41,13 @@ const About = () => {
                         <img className="celestia-logo-home object-fill " src={mission_mobile} alt="" />
                     </div>
                 </div>
-                <div className="footer w-full flex justify-around items-center relative top-[-20px] md:top-0 py-10 bg-black rounded-t-[30px] mt-10 shadow-[0_0px_15px_rgba(0,0,0,0.62)]">
-                    <Footer />
-                </div>
-                <p className="py-3 text-center text-sm">Copyright @{new Date().getFullYear()} Celestia</p>
-
 
             </div>
+            <div className="footer w-full flex justify-around items-center relative top-[-20px] md:top-0 py-10 bg-black rounded-t-[30px] shadow-[0_0px_15px_rgba(0,0,0,0.62)]">
+                <Footer />
+            </div>
+            <p className="py-3 text-center text-sm">Copyright @{new Date().getFullYear()} Celestia</p>
+
         </div>
     );
 };
